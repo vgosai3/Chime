174020efcb7e648a7592c6b0b302933862304493
mc325 <mc325temp@gmail.com>
2024-09-21 15:59:33 -0400

    Fix turret item functionality

--- Assets/Scripts/Objects/Items/TurretItem.cs
+++ Assets/Scripts/Objects/Items/TurretItem.cs
@@ -7,7 +7,8 @@ public class TurretItem : AItem
     public GameObject TurretPrefab;
     public override void PrimaryAction()
     {
-        Vector3 spawnPosition = transform.position + transform.forward * 2.0f;
+        GameObject player = GameObject.FindWithTag("Player");
+        Vector3 spawnPosition = player.transform.position + player.transform.forward * 2.0f;
         Instantiate(TurretPrefab, spawnPosition, Quaternion.identity);
 
     }
